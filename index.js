1363368302781e581ce5aa3f391b4a6480e84e5a
Yogayata Verma <yogayatajugn@gmail.com>
2025-02-18 23:33:10 +0530

    changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // React app's URL
+  origin: 'https://portfolio-frontend-nu-one.vercel.app', // React app's URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
@@ -25,7 +25,7 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://yogayatajugnu:Family_11111@cluster0.fzv45r6.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI);
 
 // Define Schemas and Models
 
